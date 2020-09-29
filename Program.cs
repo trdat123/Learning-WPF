47ff4243380b1abb159fa539f1e4316b59c31b2d
trdat123 <nkokviper123@gmail.com>
2020-09-29 16:23:33 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WindowsProgrammingExcercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
