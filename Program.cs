999824af8cb83565ba488de34299780d5c476454
trdat123 <nkokviper123@gmail.com>
2020-09-29 16:41:40 +0700

    add

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace WindowsProgrammingExcercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worlda!");
         }
     }
 }
